f8a3d0d9f823052e933cf09f07cf8f7ff8421612
elzp <86740659+elzp@users.noreply.github.com>
2023-01-19 19:08:33 +0100

    Add Input component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import { useState } from 'react';
 import './style.css';
+import Input from './Input';
 
 export default function App() {
+  const [searchValue, setSeatchValue] = useState('');
+  function onInputChange(e) {
+    console.log(e.target.value);
+  }
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
+      <Input onInputChange={onInputChange} />
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
---
+++ Input.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function Input(props) {
+  return (<input onChange={(e)=>props.onInputChange(e)}></input>)
+}
