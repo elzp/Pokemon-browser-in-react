b143b3f13d28a2dfc4307ebad8daf2b64385320e
elzp <86740659+elzp@users.noreply.github.com>
2023-01-20 18:08:51 +0100

    Update ResultBoard component with ts interaces&types

--- ResultBoard.tsx
+++ ResultBoard.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import Card from './Card';
+interface pokemonItem {
+  name: string;
+  listOfAbilities: Array<string>;
+}
 export default function ResultBoard(props) {
+  const arrayOfResults: Array<pokemonItem> = props.arrayOfResults ?? [
+    { name: '1', listOfAbilities: ['11', '12'] },
+  ];
   return (
-    {props.arrayOfResults.map(item=>{
-      return (
-        <Card name={item.name} listOfAbilities={item.listOfAbilities}/>
-      )
-    })}
+    <div>
+      {arrayOfResults.map((item: pokemonItem) => {
+        return <Card name={item.name} listOfAbilities={item.listOfAbilities} />;
+      })}
+    </div>
   );
 }
