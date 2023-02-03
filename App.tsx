8b64e6a1c6bd192d40189cae3988c62a879d534b
elzp <86740659+elzp@users.noreply.github.com>
2023-02-03 20:51:17 +0100

    Add code to immediately show  search results  based of API response

--- App.tsx
+++ App.tsx
@@ -7,39 +7,7 @@ import Button from './Button';
 
 export default function App() {
   const [searchValue, setSearchValue] = useState('');
-  const [arrayOfResults, setArrayOfResults] = useState([]);
-
-  function searchName(input) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1279`)
-      .then((response) => response.json())
-      .then((response) => {
-        const allNames = response.results;
-
-        const regex = new RegExp(`${input}`, 'i');
-        const resultOfSearch = allNames.filter((item) => regex.test(item.name));
-        return resultOfSearch;
-      })
-      .then((response) => {
-        let arrayCointainer = [];
-        response.forEach((item) => {
-          fetch(item.url)
-            .then((response) => response.json())
-            .then((response) => {
-              const pokemonData = {
-                name: response.name,
-                listOfAbilities: response.abilities.map(
-                  (item) => item.ability.name
-                ),
-                img_url: response.sprites.front_default,
-              };
-              arrayCointainer.push(pokemonData);
-            });
-        });
-        console.log('results after ferxching', arrayCointainer);
-        setArrayOfResults(arrayCointainer);
-      })
-      .catch((err) => console.error(err));
-  }
+  const [search, setSearch] = useState(false);
 
   function onInputChange(e) {
     console.log(e.target.value);
@@ -47,12 +15,12 @@ export default function App() {
   }
 
   function handleClickSearch(e) {
-    searchName(searchValue);
+    setSearch(true);
   }
 
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Explore Pokemon World!</h1>
       <Input onInputChange={onInputChange} />
       <Button
         onClickFn={(e) => {
@@ -60,7 +28,11 @@ export default function App() {
         }}
         name={'Search'}
       />
-      <ResultBoard arrayOfResults={arrayOfResults} />
+      <ResultBoard
+        search={search}
+        searchedValue={searchValue}
+        setSearch={setSearch}
+      />
     </div>
   );
 }
--- ResultBoard.tsx
+++ ResultBoard.tsx
@@ -6,29 +6,74 @@ interface pokemonItem {
   img_url: string;
 }
 export default function ResultBoard(props) {
-  const arrayOfResults: Array<pokemonItem> =
-    props.arrayOfResults.length !== 0
-      ? props.arrayOfResults
-      : [
-          {
-            name: 'clefairy',
-            listOfAbilities: ['friend-guard'],
-            img_url:
-              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png',
-          },
-        ];
+  const [arrayOfResults, setArrayOfResults] = React.useState([
+    // {
+    //   name: 'clefairy',
+    //   listOfAbilities: ['friend-guard'],
+    //   img_url:
+    //     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png',
+    // },
+  ]);
+  const [text, changeText] = React.useState('search some pokemons')
+  const { search, searchedValue } = props;
+  async function searchName(input) {
+    await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1279`)
+      .then((response) => response.json())
+      .then((response) => {
+        const allNames = response.results;
+
+        const regex = new RegExp(`${input}`, 'i');
+        const resultOfSearch = allNames.filter((item) => regex.test(item.name));
+        return resultOfSearch;
+      })
+      .then((response) => {
+        let arrayCointainer = [];
+        response.forEach((item) => {
+          fetch(item.url)
+            .then((response) => response.json())
+            .then((response) => {
+              const pokemonData = {
+                name: response.name,
+                listOfAbilities: response.abilities.map(
+                  (item) => item.ability.name
+                ),
+                img_url: response.sprites.front_default,
+              };
+              setArrayOfResults((prev) => [...prev, pokemonData]);
+            })
+            .catch((err) => console.error(err));
+        });
+      })
+      .catch((err) => console.error(err));
+  }
+
+  React.useEffect(() => {
+    if (search && searchedValue !== '') {
+      searchName(searchedValue);
+    }
+    if (search && searchedValue === '') {
+      setArrayOfResults([]);
+      changeText('search some pokemons')
+    }
+    props.setSearch(false);
+  }, [search]);
+
   return (
     <div>
-      {arrayOfResults.map((item: pokemonItem) => {
-        return (
-          <Card
-            key={item.name}
-            name={item.name}
-            listOfAbilities={item.listOfAbilities}
-            img_url={item.img_url}
-          />
-        );
-      })}
+      {arrayOfResults.length === 0 ? (
+        <div>{text}</div>
+      ) : (
+        arrayOfResults.map((item: pokemonItem, index: number) => {
+          return (
+            <Card
+              key={item.name + index}
+              name={item.name}
+              listOfAbilities={item.listOfAbilities}
+              img_url={item.img_url}
+            />
+          );
+        })
+      )}
     </div>
   );
 }
