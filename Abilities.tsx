17b0c8934b0bccebf03399935bd4f391edb46015
elzp <86740659+elzp@users.noreply.github.com>
2023-02-20 19:35:52 +0100

    Add showing ability's description on hover

--- Abilities.tsx
+++ Abilities.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
+import useHover from './hooks';
+import { LegacyRef } from 'react';
 
 export default function Abilities(props) {
   const { ability, index } = props;
+  const {
+    ref: hoverRef,
+    value: isHovered,
+  }: { ref: LegacyRef<HTMLDivElement>; value: boolean } = useHover();
   return (
     <div key={ability.name + index}>
-      <div>{ability.name}</div>
-      <div className="description">{ability.description}</div>
+      <div ref={hoverRef}>{ability.name}</div>
+      {isHovered && <div className="description">{ability.description}</div>}
     </div>
   );
 }
